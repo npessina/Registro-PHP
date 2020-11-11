d67124b01ca69749fa3fbf2463e037e8f54ad315
npessina <nico.pessina7@gmail.com>
2020-11-11 17:10:32 -0300

    Registro PHP

---
+++ views/contenido.view.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>Contenido</title>
+</head>
+<body>
+    <div class="contenedor">
+        <h1 class='titulo'>Contenido del sitio</h1>
+        <a href="cerrar.php">Cerrar Sesión</a>
+        <hr class="border">
+
+        <div class="contenido">
+            <article>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum minima nam perferendis beatae illo nemo a consectetur ab, harum fugit provident maxime cum omnis dolores vero totam earum corrupti at?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, perspiciatis eum minus ut soluta, error quibusdam in laborum assumenda porro molestias vitae dolore tempora voluptatum nobis debitis, nulla blanditiis alias?</p>
+            </article>
+        </div>
+        
+    </div>
+</body>
+</html>
